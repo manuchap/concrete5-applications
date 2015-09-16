2dac5f17d251841f406b55524d656a7087c02b99
myconcretelab <seb@myconcretelab.com>
2015-09-17 01:16:26 +0200

    update mcl new

--- myconcretelab_new/config/generated_overrides/concrete.php
+++ myconcretelab_new/config/generated_overrides/concrete.php
@@ -2,24 +2,24 @@
 
 /**
  * -----------------------------------------------------------------------------
- * Generated 2015-08-20T09:16:00+00:00
+ * Generated 2015-09-17T01:03:06+02:00
  *
- * @item      misc.latest_version
+ * @item      version_installed
  * @group     concrete
  * @namespace null
  * -----------------------------------------------------------------------------
  */
 return array(
     'site' => 'Myconcretelab',
-    'version_installed' => '5.7.3.1',
+    'version_installed' => '5.7.5.1',
     'misc' => array(
         'access_entity_updated' => 1421612609,
         'latest_version' => '5.7.3.1',
         'seen_introduction' => true,
-        'do_page_reindex_check' => false
+        'do_page_reindex_check' => false,
     ),
     'seo' => array(
-        'url_rewriting' => 1
+        'url_rewriting' => 1,
     ),
     'cache' => array(
         'blocks' => false,
@@ -28,9 +28,9 @@ return array(
         'overrides' => false,
         'pages' => '0',
         'full_page_lifetime' => 'default',
-        'full_page_lifetime_value' => null
+        'full_page_lifetime_value' => null,
     ),
     'theme' => array(
-        'compress_preprocessor_output' => true
-    )
+        'compress_preprocessor_output' => true,
+    ),
 );
